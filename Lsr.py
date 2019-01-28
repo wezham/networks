1c66e191256902283b1163b3e8c8bb49e4f2e1f3
wezham <wezham1@gmail.com>
2019-01-28 23:36:50 +1100

    node drop off and pick up seems to be working. although not flawless

--- Lsr.py
+++ Lsr.py
@@ -14,54 +14,87 @@ LINK_STATE_INTERVAL = 1
 debug = 0
 LOCALHOST = "127.0.0.1"
 
+
+######################################################
+# Graph is our network 
+######################################################
+
 class Network:
     def __init__(self, root):
         self.root_id = root
         self.routers = list()
         self.router_hash = {}
         self.root_node = ""
+        self.excluded_for_update_and_remove = []
+        self.heartbeat_hash = {}
+        self.last_valid_sequence = {}
 
-    def add_switch_by_packets(self, packet_array, identity):
-        if identity not in self.router_hash:
+    def update_last_valid_sequence(self, identifier, number):
+        self.last_valid_sequence[identifier] = number 
+
+    def handle_packet(self, packet_array, identity, sequence_num, broadcast_hash):
+        seq_num = int(sequence_num)
+        if identity not in self.router_hash: ## Add it its a new switch
             switch = Switch(identity)
             for n in packet_array:
                 neighbour_id, cost = n.split(" ", 1)
                 link_id = neighbour_id
                 switch_link = Link(link_id, float(cost))
                 switch.add_link(switch_link)
+
+            self.last_valid_sequence[identity] = 0
             self.routers.append(switch) 
             self.router_hash[identity] = switch
-        else:
+            self.heartbeat_hash[identity] = 0
+
+        else: ## Check one of two conditions
+            
+            links = [l.edge_id for l in self.router_hash.get(identity).links if l.enabled] 
             if len(packet_array) < len(self.router_hash.get(identity).links):
-                print("We have a link that has now been removed")
-                self.__find_broken_ids(identity, packet_array)
-            elif len(packet_array) > len([l for l in self.router_hash.get(identity).links if l.enabled]):
-                print("A node has come back online")
+                #print("Not enough try to remove")
+                self.__find_broken_ids(identity, packet_array, broadcast_hash)
+            elif len(packet_array) > len(links):
+                #print("Have to Add")
+                #print(f"Sequence Num {sequence_num}")
+                self.last_valid_sequence[identity] = seq_num
+                self.set_active_again(identity, packet_array)
 
-    def get_active_links(self):
-        return []
+    def set_active_again(self, identity, packet_array):
+        packet_ids = [item.split(" ", 1)[0] for item in packet_array]
+        link_ids = [link.edge_id for link in self.router_hash.get(identity).links if link.enabled]
+        to_be_enabled = list(set(packet_ids)-set(link_ids))
+        #print(f"for {identity}")
+        #print(f"enabled {link_ids}")
+        #print(f"packet {packet_ids}")
+        #print(to_be_enabled)
+        for identifier in to_be_enabled:
+            #print(self.router_hash.get(identifier))
+            #print(self.router_hash.get(identifier).links)
+            if not self.router_hash.get(identifier).enabled:
+                self.set_links_and_switch(identifier, True)
 
-    def __find_broken_ids(self, identity, packet_array):
+    def __find_broken_ids(self, identity, packet_array, broadcast_hash):
         packet_ids = [item.split(" ", 1)[0] for item in packet_array]
         link_ids = [link.edge_id for link in self.router_hash.get(identity).links]
         to_be_disabled = list(set(link_ids)-set(packet_ids))
         for identifier in to_be_disabled:
-            if self.router_hash.get(identifier).enabled:
+            if self.router_hash.get(identifier).enabled and identifier not in [l.edge_id for l in self.root_node.links]:
                 print(f"I need to disable the following {to_be_disabled}")
-                self.disable_links_and_switch(identifier)
+                self.set_links_and_switch(identifier, False)
+                broadcast_hash[identifier] = 0 
+                
 
-    def disable_links_and_switch(self, id_to_disable):
+    def set_links_and_switch(self, identifier, boolean_val):
         for switch in self.routers:
-            if switch.identity == id_to_disable:
-                print(f"Disabling Switch {switch.identity}")
-                switch.enabled = False
+            if switch.identity == identifier:
+                print(f"Setting Switch {switch.identity} to {boolean_val}")
+                switch.enabled = boolean_val
             else:
                 for link in switch.links:
-                    if link.edge_id == id_to_disable:
-                        print(f"Disabling link from {switch.identity} to {link.edge_id}")
-                        link.enabled = False
-        return True
-
+                    if link.edge_id == identifier:
+                        print(f"Setting link from {switch.identity} to {link.edge_id} to {boolean_val}")
+                        link.enabled = boolean_val
+        
     def add_root_router(self, neighbours, root):
         switch = Switch(root.id)
         self.router_hash[root.id] = switch
@@ -69,20 +102,22 @@ class Network:
             link_id = nei.id
             switch_link = Link(link_id, float(nei.cost))
             switch.add_link(switch_link)
+            self.excluded_for_update_and_remove.append(nei.id)
         self.routers.append(switch)
         self.root_node = switch
+        self.last_valid_sequence[root.id] = 0
+        self.heartbeat_hash[root.id] = 0
 
     def print_network(self):
         for switch in self.routers:
             print(f"{switch.identity} connected to:")
             for link in switch.links:
                 print(f"Link {link.edge_id} & Cost: {link.cost}") 
-            
-class Link: 
-    def __init__(self, edge_id, cost):
-        self.edge_id = edge_id
-        self.cost = cost
-        self.enabled = True
+
+
+######################################################
+# Graph Consists of Switches 
+######################################################
 
 class Switch: 
     def __init__(self, identity):
@@ -104,6 +139,20 @@ class Switch:
     def __gt__(self, other):
         return self.distance > other.distance
 
+######################################################
+# Switches consists of links 
+######################################################
+       
+class Link: 
+    def __init__(self, edge_id, cost):
+        self.edge_id = edge_id
+        self.cost = cost
+        self.enabled = True
+        
+######################################################
+# End Graph Classes
+######################################################
+
 class NRouter:
     def __init__(self, nrouter_id, port, cost):
         self.id = nrouter_id
@@ -119,7 +168,6 @@ class Router:
         self.txt_file = text_file 
         self.neighbours = list()
         self.neighbour_count = 0
-        self.heatbeat_count = 0
         self.neighbour_heartbeats = {}
         self.expected_heartbeats = {}
         self.begin_heartbeating = False
@@ -131,11 +179,15 @@ class Router:
         self.broadcast_hash = {}
         self.listen_thread= threading.Thread(target=self.listen_for_broadcast)
         self.broadcast_thread = threading.Timer(1.0, self.broadcast)
-        self.routing_thread = threading.Timer(10.0, self.routify)
+        self.routing_thread = threading.Timer(25.0, self.routify)
         self.heartbeat_thread = threading.Timer(0.5, self.__send_heartbeat)
         self.expected_beats_thread = threading.Timer(0.5, self.__check_heartbeats)
-        self.broadcast_behalf = None
         self.lock = threading.Lock()
+
+
+    ##############################################
+    # Setup Methods  
+    ##############################################
 
     def __init_neighbours(self):
         f = open(self.txt_file, "r")
@@ -157,29 +209,15 @@ class Router:
         client = socket(s.AF_INET, s.SOCK_DGRAM)
         client.bind((LOCALHOST, self.port))
         self.udp_client = client 
-
-    def send_message(self, message, port):
-        return self.udp_client.sendto(message ,(LOCALHOST, port))
     
-    def debug_packet(self):
-        base_string = self.id
-        for n in self.neighbours:
-            base_string += f"\r\n{n.id} {cost}\r\n"
+    ##############################################
+    # End Setup Methods  
+    ##############################################
 
-    def __neig_string(self):
-        base_string = ""
-        for n in self.neighbours:
-            if n.enabled:
-                base_string += f"\r\n{n.id} {n.cost}"
-        
-        self.lsp = base_string.encode()
-        return self.lsp
+    ##########################################
+    # Heartbeat related Functions 
+    ##########################################
 
-    def construct_packet(self):
-        string = f"LSP\r\n{self.id}\r\nSEQ:{self.sequence_num}".encode() + self.__neig_string()
-        self.sequence_num +=1
-        return string
-    
     def __construct_heartbeat(self):
         return f"HEARTBEAT {self.id}".encode()
 
@@ -195,24 +233,40 @@ class Router:
         self.heartbeat_thread.start()
 
     def __check_heartbeats(self):
-        print("Expected")
-        print(self.expected_heartbeats)
-        print("Actual")
-        print(self.neighbour_heartbeats)
-        print("______________________")
+        # print("Expected")
+        # print(self.expected_heartbeats)
+        # print("Actual")
+        # print(self.neighbour_heartbeats)
+        # print("______________________")
         for n in self.neighbours:
-            self.expected_heartbeats[n.id] += 1
-            if (self.expected_heartbeats[n.id]- self.neighbour_heartbeats[n.id]) >= 4 and n.enabled:
-                self.graph.disable_links_and_switch(n.id)
-                n.enabled = False
-                
+            if n.enabled:
+                self.expected_heartbeats[n.id] += 1
+                if (self.expected_heartbeats[n.id] - self.neighbour_heartbeats[n.id]) >= 4:
+                    print("Disabled links")
+                    self.graph.set_links_and_switch(n.id, False)
+                    n.enabled = False
+                    self.expected_heartbeats[n.id] = 0
+                    self.neighbour_heartbeats[n.id] = 0
+                    self.broadcast_hash[n.id] = 0
+            if not n.enabled and (self.neighbour_heartbeats[n.id] - self.expected_heartbeats[n.id] >= 4): ## node has restarted
+                print("Node has restarted")
+                n.enabled = True
+                self.neighbour_heartbeats[n.id] = self.expected_heartbeats[n.id]
+                self.graph.update_last_valid_sequence(identifier=n.id, number=0)
+                self.graph.set_links_and_switch(n.id, True)
+
         self.expected_beats_thread.cancel()
         self.expected_beats_thread = threading.Timer(0.5, self.__check_heartbeats)
         self.expected_beats_thread.start()
 
+    ##########################################
+    # End Hearbeat related functions 
+    ##########################################
+    
+    def send_message(self, message, port):
+        return self.udp_client.sendto(message ,(LOCALHOST, port))
+
     def broadcast(self):
-        if debug:
-            print("Broadcasting NOW BITCH")
         self.lock.acquire()
         packet = self.construct_packet()
         for n in self.neighbours:
@@ -222,23 +276,36 @@ class Router:
         self.broadcast_thread = threading.Timer(1.0, self.broadcast)
         self.broadcast_thread.start()
     
-    def __broadcast_on_behalf(self, packet, excepted):
-        for n in self.neighbours:
-            if n.id == excepted:
-                continue
-            else:
-                self.lock.acquire()
-                self.send_message(packet, n.port)
-                self.lock.release()
-                self.broadcast_behalf.cancel()
-                
+    def construct_packet(self):
+        string = f"LSP\r\n{self.id}\r\nSEQ:{self.sequence_num}".encode() + self.__neig_string()
+        self.sequence_num +=1
+        return string
     
-    def deconstruct_packet(self, packet):
+    def __neig_string(self):
+        base_string = ""
+        for n in self.neighbours:
+            if n.enabled:
+                base_string += f"\r\n{n.id} {n.cost}"
+        
+        self.lsp = base_string.encode()
+        return self.lsp
+        
+    ########################################################
+    # Start Listening Related functions
+    ########################################################
 
+    #### Listens for broadcasts
+    def listen_for_broadcast(self):
+        while True: 
+            if debug:
+                print("Listening for BCast")
+            packet = self.udp_client.recvfrom(1024)
+            self.deconstruct_packet(packet)
+
+    #### Takes in broadcasts and hands them to processors
+    def deconstruct_packet(self, packet):
         deconstructed = packet[0].decode().split("\r\n")
         if deconstructed[0] == "LSP":
-            if debug:
-                print(deconstructed)
             packet_id = deconstructed[1]
             try:
                 sequence_num = deconstructed[2].split(":")[1]
@@ -246,12 +313,12 @@ class Router:
                 print("Invalid format for sequence #")
                 exit()
 
-            self.graph.add_switch_by_packets(packet[0].decode().split("\r\n")[3:], packet_id)
-            if self.__should_broadcast(packet_id, sequence_num):
-                self.broadcast_behalf = threading.Timer(1.0, self.__broadcast_on_behalf,(packet[0], packet))
-                self.broadcast_behalf.start()
-                if debug:
-                    print(f"Broadcasting on behalf of {packet_id}")
+            if self.__should_broadcast(packet_id, int(sequence_num)): ## This is a new packet
+                a = packet[0].decode().split('\r\n')
+                #print(f"Broadcasting {a}, {packet}, {sequence_num}")
+                self.graph.handle_packet(packet[0].decode().split("\r\n")[3:], packet_id, sequence_num, self.broadcast_hash)
+                self.__broadcast_on_behalf(packet[0], packet)
+                
         else:
             if not self.begin_heartbeating and all(item == 0 for item in self.neighbour_heartbeats.values()):
                 self.begin_heartbeating = True
@@ -261,27 +328,42 @@ class Router:
                 print(heartbeat)
             self.neighbour_heartbeats[heartbeat[1]] += 1
 
-    def __should_broadcast(self, router_id, seq_num):
-        if router_id in self.broadcast_hash:
-            if seq_num > self.broadcast_hash.get(router_id):
-                if debug:
-                    print(f"{self.id} Broadcasting on behalf of {router_id} sn: {seq_num}")
-                self.broadcast_hash[router_id] = seq_num
+    ### Checks via sequence numbers if we should pass on a pakcet or if its been transmitted before 
+    def __should_broadcast(self, lsp_id, seq_num):
+        if lsp_id in self.broadcast_hash: ### Already seen before 
+            if seq_num > self.broadcast_hash.get(lsp_id): ### This is a not propogated 
+                self.broadcast_hash[lsp_id] = seq_num
                 return True
             else:
-                if debug:
-                    print(f"BH: {router_id}, snum: {seq_num}")
                 return False
-        else:
-            self.broadcast_hash[router_id] = seq_num
+        else: ### Never seen before ( Base case )
+            self.broadcast_hash[lsp_id] = seq_num
             return True
 
-    def listen_for_broadcast(self):
-        while True: 
-            if debug:
-                print("Listening for BCast")
-            packet = self.udp_client.recvfrom(1024)
-            self.deconstruct_packet(packet)
+    ### Broadcasts a LSP on behalf of another node 
+    def __broadcast_on_behalf(self, packet, lsp_id):
+        for n in self.neighbours:
+            if n.id == lsp_id:
+                continue
+            else:
+                self.lock.acquire()
+                self.send_message(packet, n.port)
+                self.lock.release()
+
+    ########################################################
+    # End Listening Related functions
+    ########################################################
+
+    ########################################################
+    # Start Routing Thread related functions 
+    ########################################################
+    
+    def print_route(self, path_list):
+        print(f"I am router {self.id}")
+        for switch in path_list.values():
+            if switch.identity != self.id and switch.enabled:
+                route = self.__recurse_route("", switch)
+                print(f"Least cost path to router {switch.identity}:{route} and the cost: {switch.distance}")
 
     def __fetch_queue(self):
         router_queue = list()
@@ -299,13 +381,6 @@ class Router:
         else: 
             path = node.identity + path
         return self.__recurse_route(path, node.previous)
-
-    def print_route(self, path_list):
-        print(f"I am router {self.id}")
-        for switch in path_list.values():
-            if switch.identity != self.id and switch.enabled:
-                route = self.__recurse_route("", switch)
-                print(f"Least cost path to router {switch.identity}:{route} and the cost: {switch.distance}")
     
     def __get_active_links(self, current_node):
         active_links = []
@@ -316,6 +391,8 @@ class Router:
 
     def __djikstra_algo(self):
         self.lock.acquire()
+        if self.id == "F":
+            self.graph.print_network()
         unvisited_nodes = self.__fetch_queue()
         visited = {}
         while unvisited_nodes:
@@ -344,16 +421,15 @@ class Router:
         self.print_route(self.graph.router_hash)
         self.lock.release()
 
-    def __print_djikstra(self):
-        print("Djikstra")
-        self.routing_thread.cancel()
-        self.routing_thread = threading.Timer(20.0, self.routify)
-        self.routing_thread.start()
-
     def routify(self):
         self.__djikstra_algo()
-        timer2 = threading.Timer(10.0, self.__print_djikstra) 
-        timer2.start()
+        self.routing_thread.cancel()
+        self.routing_thread = threading.Timer(25.0, self.routify)
+        self.routing_thread.start()
+    
+    ########################################################
+    # These are all functions relating to routing 
+    ########################################################
 
     def run(self):
         try: 
