3c7f31f757cb5a835237f886aebe2b0898860c6c
wezham <wezham1@gmail.com>
2019-02-02 13:07:43 +1100

    final-working

--- mytests.py
+++ mytests.py
@@ -6,7 +6,7 @@ print('if [ "$1" = "1" ]')
 print("then")
 
 for index,letter in enumerate(list(string.ascii_uppercase)[:6]):
-    print(f'osascript -e \'tell application "Terminal" to do script "python3 /Users/wesleyhamburger/Desktop/3331/assignment/networks/Lsr.py {letter} 500{index} /Users/wesleyhamburger/Desktop/3331/assignment/networks/test1/config{letter}.txt > test1.txt"\'')
+    print(f'osascript -e \'tell application "Terminal" to do script "python3 /Users/wesleyhamburger/Desktop/3331/assignment/networks/Lsr.py {letter} 500{index} /Users/wesleyhamburger/Desktop/3331/assignment/networks/test1/config{letter}.txt"\'')
 
 print("fi")
 print("#######################################################\n\n")
